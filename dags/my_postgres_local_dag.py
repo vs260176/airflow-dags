2ad0b6abc10e77e98ad20f6e822db17f0b672ece
isoo <isoo@rambler.ru>
2025-12-05 20:35:06 +0300

    addding test my_postgres_local_dag.py

---
+++ dags/my_postgres_local_dag.py
@@ -0,0 +1,17 @@
+# Минимальный тестовый DAG (файл my_postgres_local_dag.py)
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='postgres_import_test_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    
+    test_query = PostgresOperator(
+        task_id="test_connection",
+        postgres_conn_id="my_postgres_local",
+        sql="SELECT 1;"
+    )
